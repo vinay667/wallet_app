a636ff01b674cf9e8889e34fde8ddff063cfbd93
vinay667 <vinaysinghrampus466@gmail.com>
2020-05-18 15:04:10 +0530

    setup commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+class WalletHomeScreen extends StatefulWidget
+{
+  WalletHomeState createState()=>WalletHomeState();
+}
+
+class WalletHomeState extends State<WalletHomeScreen>
+{
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
+
+
+
